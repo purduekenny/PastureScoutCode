726e28f485ca36fe2e2dad5587e3c7b87bec1bb3
Angeline Tran <angeline.e.tran@gmail.com>
2012-10-14 17:58:57 -0400

    added uservoice

--- application/views/footer/main_view.php
+++ application/views/footer/main_view.php
@@ -86,6 +86,22 @@
         $("#first_name").focus();
 });
 </script>
+
+<script type="text/javascript">
+
+var uvOptions = {};
+
+(function() {
+
+var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+
+uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/sDMTrxU2lexjxWhiBc3nbA.js';
+
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+
+})();
+
+</script>
 </body>
 </html>
 
